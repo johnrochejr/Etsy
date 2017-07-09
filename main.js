71ad6191673bf317be58a8c57ee4a5d9cefbc0f9
John Roche <roche.johnjr@gmail.com>
2017-07-09 11:30:10 -0400

    Completed question 1

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
-// Since the `data.js` file is loaded in your `index.html` before this one, 
+// Since the `data.js` file is loaded in your `index.html` before this one,
 // you have access to the `data` variable that contains the Etsy Store data.
-// Open your `index.html` page in your browser and you can verify that the following
+// Open your `index.html` page in your browser and you can verify that
+// the following
 // is properly working. The `data` variable is an array with 25 items in it
+
 console.log(data);
 
-
-
 // 1: Show me how to calculate the average price of all items.
+
 function question1 () {
-  // Answer:
+  let sum = 0;
+  let prices = data.length;
+
+  for (let i = 0; i < prices; i++) {
+    sum += data[i].price;
+}
+  console.log("The average price is $" + (sum) / (prices) + ".");
 }
 
 
 
-// 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
+// 2: Show me how to get an array of items that cost between $14.00
+// and $18.00 USD
+
 function question2 () {
   // Answer:
 }
@@ -21,19 +30,22 @@ function question2 () {
 
 
 // 3: Which item has a "GBP" currency code? Display it's name and price.
+
 function question3 () {
   // Answer:
 }
 
 
 // 4: Display a list of all items who are made of wood.
+
 function question4 () {
   // Answer:
 }
 
 
-// 5: Which items are made of eight or more materials? 
+// 5: Which items are made of eight or more materials?
 //    Display the name, number of items and the items it is made of.
+
 function question5 () {
   // Answer:
 }
@@ -41,6 +53,7 @@ function question5 () {
 
 // 6: How many items were made by their sellers?
 // Answer:
+
 function question6 () {
   // Answer:
 }
